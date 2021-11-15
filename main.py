06f8faddb30a7ced65d294a201a8da6ef1a19a6d
yasinaroslav <yarik101105@yandex.ru>
2021-11-15 11:46:38 +0300

    График для построения зависимости плотности атмосферы от высоты

--- main.py
+++ main.py
@@ -1,15 +1,16 @@
 import math  # импортируем библиотеку для математических вычислений
 import matplotlib.pyplot as plt  # библиотека для построения графиков и визуалищации данных
-import numpy as np
+import numpy as np  # библиотека numpy для работы с массивами
 
 
+# функция вычисления плотности атмосферы
 def func(x):
     return math.e ** (-x / 9) * 1.2
 
 
-x = np.arange(0.0, 130.0, 0.3)
-y = np.vectorize(func)
-y = y(x)
+x = np.arange(0.0, 130.0, 0.3)  # массив значений X
+y = np.vectorize(func)  # задаем функцию y
+y = y(x)  # задаем массив значений Y
 fig = plt.figure()  # создание объекта для рисования графиков
 plt.title('Зависимость плотности атмосферы от высоты')  # заголовок
 plt.xlabel('h, км')  # подпись оси OX
